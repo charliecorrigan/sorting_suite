f5970891300901bc3c68b6f80e92be2ed10d96f2
ccorrigan <barnumswinter@gmail.com>
2017-03-15 18:36:32 -0600

    Put code into BubbleSort class

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,29 @@
-list = ["d", "b", "a", "c"]
-ordered = false
-
-
-until ordered
-    counter = 0
-    list.each_index do |index|
-        if list[index] == list[-1]
-            break
+class BubbleSort
+    def sort( list )
+        ordered = false
+        until ordered
+            counter = 0
+            list.each_index do |index|
+                if list[index] == list[-1]
+                    break
+                end
+                if list[index] > list[index + 1]
+                    swap_1 = list[index]
+                    swap_2 = list[index + 1]
+                    list[index] = swap_2
+                    list[index + 1] = swap_1
+                    counter += 1
+                end
+            end
+            if counter == 0
+                ordered = true
+            end
         end
-        if list[index] > list[index + 1]
-            swap_1 = list[index]
-            swap_2 = list[index + 1]
-            list[index] = swap_2
-            list[index + 1] = swap_1
-            counter += 1
-        end
-    end
-    if counter == 0
-        ordered = true
+        puts list
     end
 end
-puts list
+
+sorter = BubbleSort.new
+sorter.sort(["d", "b", "a", "c"])
+
 
