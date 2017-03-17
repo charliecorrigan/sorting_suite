1eead66e0a9c9b322dcc8f050a5b091852e0ae1e
ccorrigan <barnumswinter@gmail.com>
2017-03-16 19:29:33 -0600

    Fixed indentation. Changed from 4 spaces to 2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,33 +1,34 @@
 class BubbleSort
-    def sort( list )
-        ordered = false
-        until ordered
-            swaps_completed = 0
-            list.each_index do |i| 
-                if end_of_list?(list, i) then break end
-                if pair_unordered?(list, i)
-                    list[i], list[i + 1] = swap(list, i)
-                    swaps_completed += 1
-                end
-            end
-            if swaps_completed == 0 then ordered = true end
+  def sort( list )
+    ordered = false
+      until ordered
+        swaps_completed = 0
+        list.each_index do |i| 
+          if end_of_list?(list, i) then break end
+          if pair_unordered?(list, i)
+            list[i], list[i + 1] = swap(list, i)
+              swaps_completed += 1
+          end
         end
-        puts list
-    end
+        if swaps_completed == 0 then ordered = true end
+      end
+      puts list
+  end
 
-    def swap(list, i)
-        value_1 = list[i + 1]
-        value_2 = list[i]
-        return value_1, value_2
-    end
+  def swap(list, i)
+    value_1 = list[i + 1]
+    value_2 = list[i]
+    return value_1, value_2
+  end
 
-    def end_of_list?(list, i)
-        list[i] == list[-1]
-    end
+  def end_of_list?(list, i)
+    list[i] == list[-1]
+  end
 
-    def pair_unordered?(list, i)
-        list[i] > list[i + 1]
-    end
+  def pair_unordered?(list, i)
+    list[i] > list[i + 1]
+  end
+
 end
 
 sorter = BubbleSort.new
