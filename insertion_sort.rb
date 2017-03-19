2636511cf5288bd095ebe0ceb7912e8cfe7b7cd6
ccorrigan <barnumswinter@gmail.com>
2017-03-19 14:07:50 -0600

    moved code into class

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,27 +1,36 @@
+class InsertionSort
+  def sort(unsorted)
+    @unsorted = unsorted
+    @sorted      = []
+    @finished    = false
+    @current     = nil
+    @insertion_index = nil
 
-
-unsorted = [5, 8, 1, 9, 3, 20, 4, 2, 6]
-sorted = []
-finished = false
-current = nil
-magic_index = nil
-
-current = unsorted.shift
-sorted << current
-
-until finished
-  current = unsorted.shift
-  sorted.each_with_index do |sorted_item, index_of_item|
-    if current < sorted_item
-      magic_index = index_of_item
-      break
-    else
-      magic_index = -1
+    sort_first_item
+    
+    until @finished
+      @current = @unsorted.shift
+      @sorted.each_with_index do |sorted_item, index_of_item|
+        if @current < sorted_item
+          @insertion_index = index_of_item
+          break
+        else
+          @insertion_index = -1
+        end
+      end
+      @sorted.insert(@insertion_index, @current)
+      @finished = true if @unsorted.empty?
     end
+    puts @sorted
   end
-  sorted.insert(magic_index, current)
-  
-  finished = true if unsorted.empty?
+
+  def sort_first_item
+    @current = @unsorted.shift
+    @sorted << @current
+  end
 end
 
-puts "new sorted array is #{sorted}"
+sorter = InsertionSort.new
+
+sorter.sort(["d", "b", "a", "c"])
+
